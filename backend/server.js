26916ff48ceaeb2d5b6dc2b6ed9dc3023d223b29
abhigyan-p512 <abpr22ise@cmrit.ac.in>
2026-01-28 18:25:20 +0530

    Update CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: ' https://subscription-management-task-frontend.onrender.com',
   credentials: true
 }));
 
