68cfa74c34c3b2dccf500e6c6ef53d9a8659b128
abhigyan-p512 <abpr22ise@cmrit.ac.in>
2026-01-28 17:29:58 +0530

    Update PaymentMethods.js

--- frontend/src/components/PaymentMethods.js
+++ frontend/src/components/PaymentMethods.js
@@ -8,7 +8,7 @@ import {
 import axios from 'axios';
 import './PaymentMethods.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://subscription-management-task.onrender.com';
 
 function PaymentMethods({ user }) {
   const stripe = useStripe();
